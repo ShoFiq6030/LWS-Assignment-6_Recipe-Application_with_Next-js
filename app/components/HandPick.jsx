fa14a454bd5d1a2c73b1a70dc11f03a2cbf19950
shofiq6030 <mdshofiq6030@gmail.com>
2025-01-14 00:11:34 +0600

    add Hand-Picked Collections functionality

--- app/components/HandPick.jsx
+++ app/components/HandPick.jsx
@@ -1,49 +1,64 @@
 import Image from "next/image";
 import React from "react";
+import recipes from "../../data/recipes.json";
+import categories from "../../data/categories.json";
+import Link from "next/link";
 
 export default function HandPick() {
+  const sortByAvgRating = recipes
+    .sort((a, b) => {
+      return b.rating.average_rating - a.rating.average_rating;
+    })
+    .slice(0, 2);
+
   return (
     <section className="mb-16">
       <h2 className="text-3xl font-bold mb-8 animate-fade-in-down">
         Hand-Picked Collections
       </h2>
       <div className="grid md:grid-cols-2 gap-8">
-        <div className="relative group overflow-hidden rounded-lg transition-transform duration-300 ease-in-out transform cursor-pointer">
-        <div className="relative w-full h-[400px]">
-          <Image
-            src="https://images.unsplash.com/photo-1607247098731-5bf6416d2e8c?q=100&w=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Sushi Combos"
-            layout="fill"
-              objectFit="cover"
-            className=" rounded-lg object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-110"
-          />
-          </div>
-          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 rounded-b-lg transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0">
-            <h3 className="text-xl font-semibold mb-2">
-              Sushi Combos for your Next Party
-            </h3>
-            <a href="./recipes.html" className="text-orange-300 hover:underline">
-              View Collection
-            </a>
-          </div>
-        </div>
-        <div className="relative group overflow-hidden rounded-lg transition-transform duration-300 ease-in-out transform cursor-pointer">
-            <div className="relative w-full h-[400px]">
-          <Image
-            src="https://images.unsplash.com/photo-1649402206323-9bca1d77d10e?q=100&w=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Everything Bagel"
-            layout="fill"
-            objectFit="cover"
-            className="w-full h-[400px] rounded-lg object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-110"
-          />
-          </div>
-          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 rounded-b-lg transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0">
-            <h3 className="text-xl font-semibold mb-2">Everything Bagel</h3>
-            <a href="./recipes.html" className="text-orange-300 hover:underline">
-              View Collection
-            </a>
-          </div>
-        </div>
+        {sortByAvgRating.map((recipe) => {
+          const categoryName = categories.find(
+            (category) => category.id === recipe.category_id
+          )?.name;
+          return (
+            <Link
+              key={recipe.title}
+              href={{
+                pathname: `/recipeDetails/${categoryName}/${recipe.title}`,
+                query: {
+                  category_id: recipe.category_id,
+                  title: recipe.title,
+                  description: recipe.description,
+                  published_date: recipe.published_date,
+                  cooking_time: recipe.cooking_time,
+                  author: recipe.author,
+                  thumbnail: recipe.thumbnail,
+                },
+              }}
+              className="relative group overflow-hidden rounded-lg transition-transform duration-300 ease-in-out transform cursor-pointer"
+            >
+              <div className="relative w-full h-[400px]">
+                <Image
+                  src={recipe.thumbnail}
+                  alt="Sushi Combos"
+                  layout="fill"
+                  objectFit="cover"
+                  className=" rounded-lg object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-110"
+                />
+              </div>
+              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 rounded-b-lg transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0">
+                <h3 className="text-xl font-semibold mb-2">{recipe.title}</h3>
+                <a
+                  href="./recipes.html"
+                  className="text-orange-300 hover:underline"
+                >
+                  View Collection
+                </a>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
--- app/components/LatestRecipes.jsx
+++ app/components/LatestRecipes.jsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import React from "react";
+import recipes from "../../data/recipes.json";
+import categories from "../../data/categories.json";
 
 export default function LatestRecipes() {
+  const sortByDate = recipes
+    .sort((a, b) => {
+      return b.published_date - a.published_date;
+    })
+    .slice(0, 4);
+  console.log(sortByDate);
   return (
     <section className="mb-16">
       <h2 className="text-3xl font-bold mb-8">Latest Recipes</h2>
@@ -18,45 +26,6 @@ export default function LatestRecipes() {
           </div>
           <h3 className="text-lg font-semibold mb-2">Strawberry Cream</h3>
           <p className="text-gray-600">Dessert</p>
-        </div>
-        <div>
-          <div className="relative w-full h-[300px]">
-            <Image
-              src="https://plus.unsplash.com/premium_photo-1664472634106-1430c3973e68?q=100&w=1674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Creamy Garlic Pasta"
-              layout="fill"
-              objectFit="cover"
-              className="  rounded-lg mb-4"
-            />
-          </div>
-          <h3 className="text-lg font-semibold mb-2">Creamy Garlic Pasta</h3>
-          <p className="text-gray-600">Pasta</p>
-        </div>
-        <div>
-          <div className=" relative w-full h-[300px]">
-            <Image
-              src="https://images.unsplash.com/photo-1694067508035-6cccccb95cb0?q=100&w=1674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Blueberry Muffins"
-              layout="fill"
-              objectFit="cover"
-              className="  rounded-lg mb-4"
-            />
-          </div>
-          <h3 className="text-lg font-semibold mb-2">Blueberry Muffins</h3>
-          <p className="text-gray-600">Breakfast</p>
-        </div>
-        <div>
-          <div className=" relative w-full h-[300px]">
-            <Image
-              src="https://images.unsplash.com/photo-1565557623262-b51c2513a641?q=100&w=1671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Chicken Tikka Masala"
-              layout="fill"
-              objectFit="cover"
-              className="  rounded-lg mb-4"
-            />
-          </div>
-          <h3 className="text-lg font-semibold mb-2">Chicken Tikka Masala</h3>
-          <p className="text-gray-600">Dinner</p>
         </div>
       </div>
     </section>
